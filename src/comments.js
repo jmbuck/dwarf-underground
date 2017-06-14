e4730f5d89b57eee76046229c14014f57e427c4b
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-14 15:03:32 -0400

    Add timestamp to comments

--- src/comments.js
+++ src/comments.js
@@ -18,14 +18,21 @@ class Comments extends Component {
     }
 
     renderComment(comment, i) {
-         return <li key={i}>{comment}</li>
+         return (
+            <li key={i}>
+                <div className='timestamp'><strong>{comment.timestamp}</strong></div>
+                <div className='comment'>{comment.comment}</div>
+             </li>
+            )
     }
 
     handleSubmit(ev) {
         ev.preventDefault();
         if(this.commentInput.value !== '') {
+            const date = (new Date()).toString();
+            const timestamp = date.replace(date.substr(date.indexOf(':')+3), '');
             const comments = [...this.state.comments]
-            comments.unshift(this.commentInput.value)
+            comments.unshift({comment: this.commentInput.value, timestamp});
             //update state and save to localStorage
             this.setState({ comments }, () => localStorage.setItem('comments', JSON.stringify(this.state.comments)))    
             document.querySelector('.comment-box').focus();
@@ -38,14 +45,14 @@ class Comments extends Component {
             <div className="comments">
                 <form onSubmit={this.handleSubmit.bind(this)}>
                     <textarea 
-                    name="commentBox" 
-                    placeholder="Leave your comment here!"
-                    className="comment-box"
-                    ref={input => this.commentInput = input} 
+                        name="commentBox" 
+                        placeholder="Leave your comment here!"
+                        className="comment-box"
+                        ref={input => this.commentInput = input} 
                     ></textarea>
                     <button className="expanded button" type="submit">Post your comment</button>
                 </form>
-                <ul className="no-bullet">
+                <ul className="comments-list no-bullet">
                     {this.state.comments.map(this.renderComment)}
                 </ul>
             </div>
