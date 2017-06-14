a9ff02a1d434d7ba6e8438738160278d8512a8cd
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-13 22:23:53 -0400

    Move main components on page to React format

--- src/header.js
+++ src/header.js
@@ -7,12 +7,12 @@ class Header extends Component {
         <div className="expanded row header">
             <div className="clearfix">
                 <div className="float-left logo">
-                <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
+                    <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
                 </div>
                 <div className="float-right nav-links">
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Log in</a>
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Log in</a>
                 </div>
             </div>
             <div className="headline text-center small-12 columns">
